2ccc4a61375f2b954dcb3dc8f91b79f18c456198
ydkim <kimmbott11@gmail.com>
2026-03-05 20:55:50 +0900

    Initial Commit

---
+++ droptheball/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  droptheball
+//
+//  Created by 김용덕 on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ droptheball/droptheballApp.swift
@@ -0,0 +1,17 @@
+//
+//  droptheballApp.swift
+//  droptheball
+//
+//  Created by 김용덕 on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct droptheballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
